d15cd3222ae841d3b5e8800e245284c0072e701d
akshay bhat <akshayubhat@gmail.com>
2016-03-09 05:14:19 -0500

    adding DEMO

--- fabfile.py
+++ fabfile.py
@@ -78,10 +78,10 @@ def server_remote():
 
 @task
 def demo_fashion():
-    local('aws s3api get-object --bucket aub3visualsearch --key "fashion_index.tar.gz" --request-payer requester /mnt/fashion_index.tar.gz')
-    local('cd /mnt/;tar -zxvf fashion_index.tar.gz')
-    local('echo "DEMO=\'fashion_images\'" >> settings.py')
-    local('echo "INDEX_PATH=\'/mnt/fashion_images\'" >> settings.py')
+    # local('aws s3api get-object --bucket aub3visualsearch --key "fashion_index.tar.gz" --request-payer requester /mnt/fashion_index.tar.gz')
+    # local('cd /mnt/;tar -zxvf fashion_index.tar.gz')
+    local('echo "\nDEMO=\'fashion_images\'" >> settings.py')
+    local('echo "\nINDEX_PATH=\'/mnt/fashion_images\'" >> settings.py')
     local('python server.py')
 
 
